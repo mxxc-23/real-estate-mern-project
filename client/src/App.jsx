083d8220fd3284f0ac442953c1af01c839275ba8
mxxc-23 <cyprushervey23@gmail.com>
2024-02-20 13:13:40 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-gray-700 font-medium font-sans">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
