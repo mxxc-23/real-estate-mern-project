dcb4c116e1b0aca47b0b1c88c2eade92f8dfe0c0
mxxc-23 <cyprushervey23@gmail.com>
2024-02-26 23:11:45 +0800

    created backend and connected db

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const Carousel = ({ slides }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -19,14 +18,26 @@ const Carousel = ({ slides }) => {
           key={index}
           src={slide}
           alt={`Image ${index + 1}`}
-          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
+          className={`absolute top-0 left-0 w-full h-full object-none bg-black transition-opacity duration-500 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
         />
       ))}
       <button className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-full p-2" onClick={goToPreviousImage}>
-        <BsFillArrowLeftCircleFill />
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6 text-gray-600"
+         >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7"/>
+         </svg>
       </button>
       <button className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-full p-2" onClick={goToNextImage}>
-        <BsFillArrowRightCircleFill />
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6 text-gray-600"
+         >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
+         </svg>
       </button>
     </div>
   );
--- client/src/pages/Property.jsx
+++ client/src/pages/Property.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from '../components/Header';
 import Carousel from '../components/Carousel';
 
@@ -7,21 +6,35 @@ const Property = () => {
     "https://i.pinimg.com/564x/ca/e1/81/cae18106efc6156deb3ea857a9c7242e.jpg",
     "https://i.pinimg.com/564x/57/27/32/5727323b42a554b246a385c1635f6a52.jpg",
     "https://i.pinimg.com/564x/1d/fa/13/1dfa1311ac658bd38bf5ab6fecb95206.jpg",
-    "https://i.pinimg.com/564x/1d/fa/13/1dfa1311ac658bd38bf5ab6fecb95206.jpg"
+    
   ];
 
   return (
     <main>
       <Header />
       <section className="flex justify-center">
-        <div className="max-w-screen-lg w-full px-6 mt-10">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 bg-white border border-gray-200 rounded-lg shadow-lg p-6" style={{ height: '600px' }}>
+        <div className="max-w-screen-xl mt-10 px-6">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 bg-white border border-gray-200 rounded-lg shadow-lg p-6 md:min-h-full" style={{height: '80vh'}}>
             <div className="h-full">
               <Carousel slides={images} />
             </div>
             <div className="p-4">
-              <h2 className="text-2xl font-bold mb-4">Property Description</h2>
-              <p className="text-gray-700 dark:text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mattis justo et erat accumsan rutrum.</p>
+              <h2 className="text-2xl font-bold mb-4">Property Title</h2>
+              <p className="text-gray-700 dark:text-gray-300">
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                Nullam mattis justo et erat accumsan rutrum.
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit. 
+                Nullam mattis justo et erat accumsan rutrum.
+              </p>
+              <br/>
+              <h3>Property Features:</h3>
+              <ul>
+                <li>2 Bedroom</li>
+                <li>1 Comfort Room</li>
+                <li>1 Carpark</li>
+              </ul>
             </div>
           </div>
         </div>
