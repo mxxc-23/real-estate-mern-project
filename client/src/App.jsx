c3de1f9687f31f5626261190d57c595aa0b5f99d
mxxc-23 <cyprushervey23@gmail.com>
2024-04-19 11:58:08 +0800

    updated frontend pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search'
-import Header from './components/Header'
 import SouthprimeFlats from './pages/SearchByDeveloper/SouthprimeFlats'
 import Tial from './pages/Tial'
 import WVTowers from './pages/SearchByDeveloper/WVTowers'
 import EmeraldEstate from './pages/SearchByDeveloper/EmeraldEstate'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import AdminView from './pages/AdminView'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+    
     <Routes>
       <Route path="/" element={<Home />}/>
+
       <Route path="/developer/the-southprime-flats" element={<SouthprimeFlats />}/>
       <Route path="/developer/wv-towers" element={<WVTowers />}/>
       <Route path="/developer/emerald-estate" element={<EmeraldEstate />}/>
+
       <Route path="/search" element={<Search />}/> 
+
       <Route path="/test-ui" element={<Tial/>}/>
+
+      <Route path="/admin/signup" element={<SignUp/>}/>
+      <Route path="/admin/signin" element={<SignIn/>}/>
+      <Route path="/admin/dashboard" element={<AdminView/>}/>
     </Routes>
     </BrowserRouter>
     
