7c6bc39c304bfcbf310bd949521a84fc0b1073eb
mxxc-23 <cyprushervey23@gmail.com>
2024-04-19 11:54:52 +0800

    added routers in main file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import cors from "cors";
+
 import userRouter from "./routes/user.routes.js";
+import developerRouter from "./routes/developer.routes.js";
+import authRouter from "./routes/auth.route.js";
+import propertyRouter from "./routes/property.routes.js";
 
 dotenv.config();
 
@@ -12,6 +17,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express();
+app.use(express.json());
+
+app.use(cors())
 
 app.listen(3001, () => {
    console.log("Server listening on port 3001!");
@@ -19,4 +27,17 @@ app.listen(3001, () => {
 
 //request: from the client that you give to the server
 //response: from the server that you give to the client
-app.use('/api/test', userRouter)
+app.use(userRouter)
+app.use(developerRouter)
+app.use(authRouter)
+app.use(propertyRouter)
+
+app.use((error, request, response, next) => {
+   const statusCode = error.statusCode || 500;
+   const message = error.message || "Internal server error";
+   return response(statusCode).json({
+      success: false,
+      statusCode,
+      message
+   })
+})
